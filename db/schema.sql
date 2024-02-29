0013839df5c011462df9447e80527657ad8fe50e
unknown <helenvcolon@gmail.com>
2024-02-28 22:58:31 -0500

    Created schema.sql in db folder

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- This SQL script is used to drop an existing database if it exists and then create a new database.
+
+-- Drop the database named "techblog_db" if it exists
+DROP DATABASE IF EXISTS techblog_db;
+
+-- Create a new database named "techblog_db"
+CREATE DATABASE techblog_db;
